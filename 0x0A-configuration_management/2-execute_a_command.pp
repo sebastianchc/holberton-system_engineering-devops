833303091f742e4ed05e509ddec0fd146bd362b0
sebastianchc <1200@holbertonschool.com>
2020-03-24 10:19:36 -0500

    [Update] Checkers

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # kill a process named killmenow
 exec { 'Terminated':
-  path     => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
-  command  => 'pkill killmenow',
-  provider => 'shell',
+  command => 'pkill killmenow',
+  path    => '/usr/bin/',
+  returns => [0,1],
 }
