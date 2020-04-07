1cca78df3767ec216c55c68025f7351fe7a43f27
sebastianchc <1200@holbertonschool.com>
2020-04-07 13:54:48 -0500

    [Update] Understanding SSH

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,9 @@
 # Setup SSH config file
-file_line { 'No password authentication':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '#    PasswordAuthentication no'
+file_line { 'Turn off passwd auth':
+    path   => '/etc/ssh/ssh_config',
+    line   => '#    PasswordAuthentication no'
 }
-file_line { 'Adding identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '#    IdentityFile ~/.ssh/holberton'
+file_line { 'Declare identity file':
+    path   => '/etc/ssh/ssh_config',
+    line   => '#    IdentityFile ~/.ssh/holberton'
 }
