ad7f08b2a48fda1ec9d57604019b3687ea06ec4e
sebastianchc <1200@holbertonschool.com>
2020-04-06 19:44:48 -0500

    [Update] Understanding puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,5 @@
-# Create Puppet file
+# Creates a file
 file { '/tmp/holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
