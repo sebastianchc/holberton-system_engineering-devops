d14abb80ece6a2f36ff8465b8ebf161e096b8e7b
sebastianchc <1200@holbertonschool.com>
2020-04-09 17:00:49 -0500

    [Update] Adding HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,19 @@
+# Add HTTP header
+exec { 'Update' :
+     command => '/usr/bin/apt-get -y update'
+}
+
+exec { 'Install' :
+     require => Exec['Update'],
+     command => '/usr/bin/apt-get -y install nginx'
+}
+
+exec { 'Header' :
+     require => Exec['Install'],
+     command => '/bin/sed -i "s/server_name _;/server_name _;\n\taddheader X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default'
+}
+
+exec { 'Start' :
+     require => Exec['Header'],
+     command => '/usr/sbin/service nginx start'
+}
