569be9a860d2c20d0940b42a1f1e7aa5b6d2bf15
sebastianchc <1200@holbertonschool.com>
2020-05-07 22:32:04 -0500

    Only requests

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
 #!usr/bin/python3
 """Request from Reddit API"""
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     header = {"User-Agent": "Chrome"}
-    subreddit = get(url, headers=header, allow_redirects=False).json()
+    subreddit = requests.get(url, headers=header, allow_redirects=False).json()
     if subreddit.get("error") == 404:
         return 0
     else:
