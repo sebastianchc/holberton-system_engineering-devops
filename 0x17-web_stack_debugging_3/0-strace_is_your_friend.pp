3f0a1b7b71237cbfa5dd812ba4442043f19c529d
sebastianchc <1200@holbertonschool.com>
2020-05-20 13:07:46 -0500

    Only sed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # Web stack debugging 3
 exec { 'fix-wordpress':
-     command => '/bin/sed -i "s/phpp/php/" /var/www/html/wp-settings.php; /usr/sbin/service apache2 restart'
+     command => '/bin/sed -i "s/phpp/php/" /var/www/html/wp-settings.php'
 }
